d85b76cbcd6284e3b8df0418c40988ba42dfd21d
Simony-Stock <stock.simony@gmail.com>
2022-06-09 16:35:36 -0400

    Analysis GUI step 1, 2, 3 messages working

--- Analysis GUI.py
+++ Analysis GUI.py
@@ -1,12 +1,9 @@
 from multiprocessing.connection import wait
 import os
-import threading
 from time import *
-from tkinter import *     # from tkinter import Tk for Python 3.x
+from tkinter import *  
 from tkinter.filedialog import askopenfilename
 import string
-from tkinter.ttk import Progressbar
-from turtle import onclick
 import Eye_Gaze
 
 #combine all processes into one. say "Processing..." and the "Complete"
@@ -19,14 +16,26 @@ os.chdir(path_parent) #changes working directory to path_parent
 def MyButtonClicked():
   global filename
   global status1
+  global status2
+  global status3
 
   filepath = askopenfilename() # show an "Open" dialog box and return the path to the selected file
   filename = filepath.split("/")[-1]
 
-  lbl1=Label(p4, text='Data Analysis Status: ', fg='black', font=("Helvetica", 9))
+  lbl1=Label(p4, text='Step 1 - Raw Data Capture: ', fg='black', font=("Helvetica", 9))
   lbl1.pack(side = LEFT)
   status1 = Label(p4, text='Waiting', fg='red', font=("Helvetica", 9))
   status1.pack(side = LEFT)
+
+  lbl2=Label(p5, text='Step 2 - Data Processing: ', fg='black', font=("Helvetica", 9))
+  lbl2.pack(side = LEFT)
+  status2 = Label(p5, text='Waiting', fg='red', font=("Helvetica", 9))
+  status2.pack(side = LEFT)
+
+  lbl3=Label(p6, text='Step 3 - Data Visualization: ', fg='black', font=("Helvetica", 9))
+  lbl3.pack(side = LEFT)
+  status3 = Label(p6, text='Waiting', fg='red', font=("Helvetica", 9))
+  status3.pack(side = LEFT)
 
   #text- filename.ext
   lbl=Label(p2, text=str(filename), fg='black', font=("Helvetica", 9))
@@ -36,21 +45,29 @@ def MyButtonClicked():
   btn=Button(p3, text="Submit", fg='red', command = lambda: StartAnalysis()) #lambda means only run function after button is pressed and lets you pass in arguments
   btn.pack(side = LEFT)
   
-
 def StartAnalysis():
   global filename
   global status1
+  global status2
+  global status3
 
-  #gazeFile = Eye_Gaze.getGaze(filename.split(".")[0], filename.split(".")[1]) #passes in the video file and returns the gaze csv file name
-  #status1.destroy()
-  #status1=Label(p4, text='Complete', fg='green', font=("Helvetica", 9))
-  #status1.pack(side = LEFT)
+  #Step 1 ------------------------------------------------------------------------------------
   status1 = Processing(p4,status1)
-  Test()
+  #gazeFile = Eye_Gaze.getGaze(filename.split(".")[0], filename.split(".")[1]) #passes in the video file and returns the gaze csv file name
   Complete(p4,status1)
 
-  fixFile = ''
-  analysisFile = ''
+  #Step 2 ------------------------------------------------------------------------------------
+  status2 = Processing(p5,status2)
+  fixFile = Eye_Gaze.getGaze(filename.split(".")[0], filename.split(".")[1]) #passes in the video file and returns the gaze csv file name
+  Complete(p5,status2)
+
+  #Step 3 ------------------------------------------------------------------------------------
+  status3 = Processing(p6,status3)
+  #analysisFile = Eye_Gaze.getGaze(filename.split(".")[0], filename.split(".")[1]) #passes in the video file and returns the gaze csv file name
+  Complete(p6,status3)
+
+  lbl=Label(p7, text='Results stored in ' + str(fixFile), fg='black', font=("Helvetica", 9))
+  lbl.pack(side = BOTTOM)
 
 def Processing(panel, label):
   label.destroy()
@@ -63,9 +80,7 @@ def Complete(panel, label):
   label.destroy()
   label=Label(panel, text='Complete', fg='green', font=("Helvetica", 9))
   label.pack(side = LEFT)
-
-def Test():
-  gazeFile = Eye_Gaze.getGaze(filename.split(".")[0], filename.split(".")[1]) #passes in the video file and returns the gaze csv file name
+  Tk. update_idletasks(window)
 
 window = Tk() #open window
 p1 = PanedWindow()
@@ -85,6 +100,9 @@ p5.pack(side = TOP)
 
 p6 = PanedWindow()
 p6.pack(side = TOP)
+
+p7 = PanedWindow()
+p7.pack(side = TOP)
 
 #text- Please select video file
 lbl=Label(p1, text="Please select video file: ", fg='black', font=("Helvetica", 14))
