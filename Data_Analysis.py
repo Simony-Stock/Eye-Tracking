addcd265b3fcfef36617bfcb0649fe1b190a414c
Simony-Stock <stock.simony@gmail.com>
2022-07-18 14:14:20 -0400

    Complete Version 2

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -5,9 +5,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
 import numpy as np
-from matplotlib import cm as CM
 
 def getAnalysis(fileName, ext):
+
+    secondFileName = fileName +"It2"
+    videoname = str(fileName).split("fix")[0]
+
     def heatmap_AOI(dataframe):
         cmap = 'Blues'
         g = sb.heatmap(dataframe, yticklabels=False, cmap = cmap)
@@ -21,11 +24,14 @@ def getAnalysis(fileName, ext):
         #set title of heatmap
         g.set_title('Heatmap')
         plt.tight_layout()
+        #save AOI heatmap as image
+        plt.savefig(str(videoname)+'AOI_Heatmap.png')
         #display plot
         plt.show()
+    
 
     # save data from csv file as dataframe
-    df = pd.read_csv(str(fileName) + '.' + str(ext))
+    df = pd.read_csv(str(secondFileName) + '.' + str(ext))
 
     #drop all rows with AOI = 0
     df = df[df.AOI != 0]
@@ -47,22 +53,63 @@ def getAnalysis(fileName, ext):
     heatmap_AOI(dfAOI)
 
     #SCATTER PLOT/HEATMAP
+    dfPts = pd.read_csv(str(fileName) + '.' + str(ext))
 
     #convert dataframe col/series to numpy array
-    left_x_array = df['Left X'].to_numpy()
-    left_y_array = df['Left Height'].to_numpy()
-    right_x_array = df['Right X'].to_numpy()
-    right_y_array = df['Right Height'].to_numpy()
+    left_x_array = dfPts['Left X'].to_numpy()
+    left_y_array = dfPts['Left Height'].to_numpy()
+    right_x_array = dfPts['Right X'].to_numpy()
+    right_y_array = dfPts['Right Height'].to_numpy()
+
+    #account for flipped screen, switch negative <--> positive
+    left_x_array = [abs(left_x_array[n]) if (left_x_array[n] < 0) else -(left_x_array[n]) for n in range(0, len(left_x_array))]
 
     def heatmap_by_point(x_array, y_array):
 
-        heatmap, xedges, yedges = np.histogram2d(x_array, y_array, bins=20)
+        heatmap, xedges, yedges = np.histogram2d(x_array, y_array, bins=20, range=[[-0.4, 0.4], [0, 0.4]])
         extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
         
         plt.clf()
-        plt.imshow(heatmap.T, extent=extent, origin='lower', cmap='Blues')
+        plt.imshow(heatmap.T, extent=extent, origin='lower', cmap='plasma_r')
         cb = plt.colorbar()
+        plt.title('Frequency of Eye Gaze Points')
+        plt.ylabel('Normalized Eye Height')
+        plt.xlabel('Pupil Position')
+        plt.text(-0.37, 0.35, 'AOI 1')
+        plt.text(0.3, 0.35, 'AOI 2')
+        plt.text(-0.37, 0.05, 'AOI 3')
+        plt.text(0.3, 0.05, 'AOI 4')
+        #save scatter heatmap as image
+        plt.savefig(str(videoname) + 'Scatter_Heatmap.png')
         plt.show()
+        
 
     #create heatmap by points using function
     heatmap_by_point(left_x_array, left_y_array)
+
+    #AOI OVER TIME
+
+    # save data from csv file as dataframe
+    sourceFileName = secondFileName
+    df2 = pd.read_csv(sourceFileName + '.' + str(ext))
+
+    yAxis = df2['AOI'].tolist()
+    xAxis = df2['Timestamp'].tolist()
+    plt.plot(xAxis,yAxis)
+    plt.title('Eye Fixation Points ' + '(Source: ' + str(videoname) + ')')
+    plt.xlabel('Time (ms)')
+    plt.ylabel('AOI (Areas of Interest)')
+    plt.yticks([0, 1, 2, 3, 4], [0, 1, 2, 3, 4])
+    #save scatter heatmap as image
+    plt.savefig(str(videoname) + 'AOIvsTime_LineGraph.png')
+    plt.show()
+
+
+    plt.scatter(xAxis, yAxis)
+    plt.title('Eye Fixation Points ' + '(Source: ' + str(videoname) + ')')
+    plt.xlabel('Time (ms)')
+    plt.ylabel('AOI (Areas of Interest)')
+    plt.yticks([0, 1, 2, 3, 4], [0, 1, 2, 3, 4])
+    #save scatter heatmap as image
+    plt.savefig(str(videoname) + 'AOIvsTime_Scatter.png')
+    plt.show()
--- Eye_Fixation.py
+++ Eye_Fixation.py
@@ -6,38 +6,37 @@ import os
 def getFix(filename, ext):
 
     inputFileName = filename;
-    outputFileName = str(inputFileName) +"fix";
+    outputFileName = inputFileName +"fix";
 
     #Read CSV file into DataFrame df
-    df = pd.read_csv(str(inputFileName)+'.'+str(ext))
+    df = pd.read_csv(inputFileName + "." + ext)
         
-    leftXThreshold = 0 #threshold value for the x coordinate of left eye #changed this 
+    leftXThreshold = -0.135 #threshold value for the x coordinate of left eye #changed this 
     leftXMINThreshold = -0.4 #threshold value for the extreme x coordinate of left eye
     leftXMAXThreshold = 0.3 #threshold value for the extreme maximum x coordinate of left eye
 
-    rightXThreshold = 0 #threshold value for the x coordinate of right eye
-    rightXMINThreshold = -0.4 #threshold value for the extreme minumum x coordinate of right eye
-    rightXMAXThreshold = 0.3 #threshold value for the extreme maximum x coordinate of right eye
+    rightXThreshold = 0.127 #threshold value for the x coordinate of right eye
+    rightXMINThreshold = -0.3 #threshold value for the extreme minumum x coordinate of right eye
+    rightXMAXThreshold = 0.5 #threshold value for the extreme maximum x coordinate of right eye
 
-    leftYThreshold = 0.297 #threshold value for theshold height of the eye between upper and lower
-    leftYMINThreshold = 0.2 #threshold value for the extreme minimum y height of left eye
-    leftYMAXThreshold = 0.35 #threshold value for the extreme maximum y height of left eye
+    leftYThreshold = 0.3028 #threshold value for theshold height of the eye between upper and lower
+    leftYMINThreshold = 0.1 #threshold value for the extreme minimum y height of left eye
+    leftYMAXThreshold = 0.5 #threshold value for the extreme maximum y height of left eye
 
-    rightYThreshold = 0.297 #threshold value for theshold height of the eye between upper and lower
-    rightYMINThreshold = 0.2 #threshold value for the extreme minimum y height of right eye
-    rightYMAXThreshold = 0.35 #threshold value for the extreme maximum y height of right eye
+    rightYThreshold = 0.2955 #threshold value for theshold height of the eye between upper and lower
+    rightYMINThreshold = 0.1 #threshold value for the extreme minimum y height of right eye
+    rightYMAXThreshold = 0.5 #threshold value for the extreme maximum y height of right eye
 
     #function that checks if the value in the spcified column of the CSV is greater than the desired threshold value
     #this will corespond with looking in the left half of the screen
     #2 - LEFT half of the screen, 1- RIGHT half of the screen, 0- NEITHER or UNKNOWN
     def findCol(left, right):
         #set variables-----------------------------------------------------------------------------------------
-        LisCol0 = df[left] != np.empty 
+        LisCol0 = df[left] != np.empty
         #converting the df left column into a series to apply the series.between function to compare < and > simulataneously
         leftcol = df[left].squeeze()
         LisCol1 = leftcol.between(leftXThreshold, leftXMAXThreshold, inclusive="neither") #creates boolean column based on right X value (left)
         LisCol2 = leftcol.between(leftXMINThreshold, leftXThreshold, inclusive="neither") #creates boolean column based on right X value (right)
-
 
         RisCol0 = df[right] != np.empty
         #converting the df right column into a series to apply the series.between function to compare < and > simulataneously
@@ -89,25 +88,46 @@ def getFix(filename, ext):
     #function to identify the AOI (quadrant) based on the row and column caluclated by the findCol and findRow functions
     #Any row with the column and row within the left and right eye that are not equal will be removed and AOI set to 0
     def AOIid(LeftCol, RightCol, LeftRow, RightRow, Time):
+        #condition setup-----------------------------------------------------------------------------------------------
+        timeEqualZero = (df[Time] == 0) #AOI will be 0 if timestamp is equal to zero to remove error points
+        timeLessThan5 = (df[Time] < 5000 ) #remove points before 5 seconds
+        timeMoreThan5 = (df[Time].index > (len(df.index)-131)) #remove points 5 second from the end of the video using a negative indexed numpy array    
         colunEqual = (df[LeftCol] != df[RightCol]) #left and right column unequal
         rowunEqual = (df[LeftRow] != df[RightRow]) #left and right row unequal
-        timeEqualZero = (df[Time] == 0) #AOI will be 0 if timestamp is equal to zero to remove error points
-        #colEqual = (df[LeftCol] == df[RightCol]) #left and right column equal
-        #rowEqual = (df[LeftRow] == df[RightRow]) #left and right row equal
+
         condition1 = ((df[LeftCol] == 1) & (df[LeftRow] == 1)) #quadrant 1, upper left conditon
         condition2 = ((df[LeftCol] == 2) & (df[LeftRow] == 1)) #quadrant 2, upper right conditon
         condition3 = ((df[LeftCol] == 1) & (df[LeftRow] == 2)) #quadrant 3, lower left conditon
         condition4 = ((df[LeftCol] == 2) & (df[LeftRow] == 2)) #quadrant 4, lower right conditon
 
-        df['AOI'] = np.select([timeEqualZero, colunEqual, rowunEqual, condition1, condition2, condition3, condition4], [0,0,0,1, 2, 3, 4], default=np.nan)
+        #conditional statement for AOI column
+        df['AOI'] = np.select([timeMoreThan5, timeLessThan5, timeEqualZero, colunEqual, rowunEqual, condition1, condition2, condition3, condition4], [0,0,0,0,0,1, 2, 3, 4], default=0)
 
     AOIid('Lcolumn', 'Rcolumn', 'Lrow', 'Rrow', 'Timestamp') #change the value of AOI column based on the value of row and column
 
-    # Show the loaded and edited data from the dataframe in the terminal
-    print(df)
+    df.to_csv(outputFileName+'.csv') #print the updated Dataframe to the output csv file
 
-    #print the updated Dataframe to the output csv file
-    df.to_csv(outputFileName+'.csv')
+
+    #function to identify areas of fixation by averaging the identified AOIs while removing any 0 terms
+    def eyeFixation (LeftCol, RightCol, LeftRow, RightRow, Time, AOI):
+        df.drop(df[df[AOI] == 0].index, inplace=True) #remove rows with 0 AOI to calculate the fixation averages
+        
+        #determine the mode of evergy 3 rows of df  
+        fixation =df.groupby(np.arange(len(df)) // 3)[Time, AOI].apply(lambda x: x.mode()) #groups
+        fixation.to_csv(outputFileName+'It1.csv') #print the updated Dataframe to the output csv file
+        
+        #second iteration taking the mode of every 3 again to determine fixation points and relative time
+        df1 = pd.read_csv(outputFileName+'It1.csv') #create a fixation dataframe for using the mode of the first
+
+        df1= df1.dropna() #remove rows with NAN AOI to calculate the 1/3 the data set, leaving the fixation averages
+
+        fixation1 = df1.groupby(np.arange(len(df1)) // 3)[Time, AOI].apply(lambda x: x.mode()) #groups the data into sets of 3 and calculates the mode of the AOI 
+        fixation1= fixation1.dropna() #remove rows with NAN AOI to calculate the 1/3 the data set, leaving the fixation averages
+        fixation1.to_csv(outputFileName+'It2.csv') #print the updated Dataframe to the output csv file to display areas of fixation
+
+        print(fixation1) #print identified areas of eye fixation and timing
+
+    eyeFixation('Lcolumn', 'Rcolumn', 'Lrow', 'Rrow', 'Timestamp', 'AOI')
 
     #returning the csv file name
     result = str(outputFileName + '.csv')
